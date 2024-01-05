3bb6655d426a53a15cf529eae5c5a8a625be8058
citrus327 <citrus327@outlook.com>
2024-01-05 14:03:46 +0800

    feat: initial release

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-export const foo = 'foo'
+/**
+ * partition an Array into two.
+ *
+ * returns a Tuple which contain the desired elements and the rests
+ *
+ * @param arr the original array
+ * @param partitionFn a function that pick the desired elements
+ * @returns
+ */
+export const partition = <T>(
+  arr: Array<T>,
+  partitionFn: (element: T) => boolean,
+): [Array<T>, Array<T>] => {
+  const length = arr.length
+  const target: Array<T> = []
+  const rest: Array<T> = []
+
+  for (let i = 0; i < length; i++) {
+    const curr = arr[i]
+
+    if (Boolean(partitionFn(curr))) {
+      target.push(curr)
+    } else {
+      rest.push(curr)
+    }
+  }
+
+  return [target, rest]
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,20 @@
 import { it, describe, expect } from "vitest"
-import { foo } from "../src"
+import { partition } from "../src"
 
 describe("simple", () => {
   it("should equal", () => {
-    expect(foo).toBe("foo")
+    const arr = [1, 2, 3, 4, 5, 6]
+    const [target, rest] = partition(arr, (o) => o % 2 === 0)
+
+    expect(target).toEqual([2, 4, 6])
+    expect(rest).toEqual([1, 3, 5])
+  })
+
+  it("should partition with empty array", () => {
+    const arr = [1, 2, 3, 4, 5, 6]
+    const [target, rest] = partition(arr, (o) => Boolean(o))
+
+    expect(target).toEqual([1, 2, 3, 4, 5, 6])
+    expect(rest).toEqual([])
   })
 })
